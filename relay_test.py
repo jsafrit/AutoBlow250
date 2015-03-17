466253a52ecf2fefc6af7231e6abe09447951903
John Safrit <jsafrit@lmgholdings.com>
2015-03-17 13:30:43 -0400

    Comment cleanup

--- relay_test.py
+++ relay_test.py
@@ -18,15 +18,6 @@ except ValueError:
     print('Value Error: Not a valid relay.')
     sys.exit(1)
 
-# # find the first relay board
-# for device in list_of_devices:
-#     if device.decode().startswith('A'):
-#         print('Found one: : {}'.format(device.decode()))
-#         relay_board_index = list_of_devices.index(device)
-#         break
-#     else:
-#         print('Nope')
-
 # open board and setup for Async BitBing Mode
 try:
     my_relay = ft.open(relay_board_index)
@@ -53,24 +44,3 @@ time.sleep(5)
 my_relay.write(ALL_OFF)
 
 my_relay.close()
-
-
-# ul = c_ulong()
-# pul = pointer(ul)
-#
-# lib_ftdi = WinDLL('ftd2xx')
-# print('List devices...')
-# c = c_ulong(0x80000000)
-# result = lib_ftdi.FT_ListDevices(pul, None, c)
-# if not result:
-#     print('Devices found:', ul.value)
-# else:
-#     print('No devices found')
-#
-# print('\nOpening...')
-# my_relay = c_int(2)
-# print('Result:', lib_ftdi.FT_Open(my_relay, pul))
-# print(ul)
-#
-# print(lib_ftdi.FT_CreateDeviceInfoList(pul))
-# print(ul)
